c5780228de46f621ac775e2fe27b1a99827e9f39
Shitanshu Tiwari <shitanshutiwari@gmail.com>
2026-04-13 23:46:42 +0530

    feat(hero): import useNavigate, useDispatch, and useSelector hooks

--- client/src/HomePage/Hero.jsx
+++ client/src/HomePage/Hero.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setAuthSidebarOpen } from "../utils/userSlice";
 import LazyImage from "../LazyLoading/LazyImage";
 
 const Hero = () => {
